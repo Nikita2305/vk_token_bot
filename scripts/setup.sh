45ac814e06083b057c7fb471d4c6ed0d8664e6be
Nikita Nikitin <nikitanikitin2305@gmail.com>
2023-03-06 20:07:07 +0300

    fix: typo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; cd .. ; pwd -P )"
-mkdir $ROOT/sigma_bot/state
+mkdir $ROOT/vk_token_bot/state
 python3 -m venv $ROOT/env
 $ROOT/env/bin/python3 -m pip install -r $ROOT/requirements.txt
